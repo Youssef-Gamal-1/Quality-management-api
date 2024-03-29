6f02ef020672e988c3a6578f1560a7e47f71b04e
youssef <yg9552753@gmail.com>
2024-03-29 19:54:54 +0200

    loginAuth controller and route

---
+++ app/Http/Controllers/admin/auth/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\admin\auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\StoreRequest;
+use App\Models\User;
+use Cassandra\Exception\ValidationException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|min:5|max:255',
+            'password' => 'required|min:8|max:255'
+        ]);
+        $user = User::where('academic_email',$validated['email'])
+            ->orwhere('email',$validated['email'])
+            ->orWhere('username',$validated['email'])
+            ->orWhere('phone',$validated['email'])
+            ->first();
+
+        if(!$user){
+            return response()->json([
+                'msg' => 'Invalid Credentials'
+            ]);
+        }
+        if($user->QM !== 1 || !Hash::check($request->password,$user->password)){
+            return response()->json([
+                'msg' => 'Invalid Credentials'
+            ]);
+        }
+
+        return response()->json([
+            'data' => $user,
+            'token' => $user->createToken('api_token')->plainTextToken
+        ]);
+
+    }
+}
--- routes/admin/api.php
+++ routes/admin/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login',[AuthController::class,'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/users',\App\Http\Controllers\admin\UserController::class);
+    Route::apiResource('/programs',\App\Http\Controllers\admin\ProgramController::class);
 });
 
-Route::apiResource('/users',\App\Http\Controllers\admin\UserController::class);
-Route::apiResource('/programs',\App\Http\Controllers\admin\ProgramController::class);
 
 
