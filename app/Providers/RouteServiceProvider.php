cf56a74352675eb6695d8e2a7dc9176177663db0
Youssef <yg9552753@gmail.com>
2024-04-30 20:34:34 -0400

    last update

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,6 +27,10 @@ class RouteServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
+//        Route::middleware('web')
+//            ->prefix('/')
+//            ->group(base_path('routes/web.php'));
+
         Route::middleware('api')
             ->prefix('api/user')
             ->group(base_path('routes/user/api.php'));
--- routes/admin/api.php
+++ routes/admin/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\admin\CourseController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/login',[AuthController::class,'login']);
+Route::post('/login',[AuthController::class,'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/users',\App\Http\Controllers\admin\UserController::class);
